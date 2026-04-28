d4c0a56ceb0885a1f086f442a0e7a93c99fd28d0
Jawad656567 <ja289327@gmail.com>
2026-04-28 06:34:32 +0500

    new

--- FrontEnd/src/context/themeContext.jsx
+++ FrontEnd/src/context/themeContext.jsx
@@ -3,24 +3,24 @@ import { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  // Initial dark theme
+  // Default theme dark
   const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
 
-    // Agar saved theme hai to use karo
     if (savedTheme) {
       setTheme(savedTheme);
       document.documentElement.setAttribute("data-theme", savedTheme);
     } else {
-      // Default dark theme
+      // Agar kuch save nahi to dark hi rahe
       document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
     }
   }, []);
 
   const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
+    const newTheme = theme === "dark" ? "light" : "dark";
 
     setTheme(newTheme);
     localStorage.setItem("theme", newTheme);
